322e25fa3bacb8940f1ebc5cc19b5bfcaa8051ce
Narek Galstyan <narekgalstyan92@gmail.com>
2022-06-20 12:18:07 -0700

    DynamicArray and DynamicArrayDemo full homework version 1.1

--- src/homework/dynamicArray/DynamicArray.java
+++ src/homework/dynamicArray/DynamicArray.java
@@ -13,16 +13,14 @@ public class DynamicArray {
 
     //Գրել մեթոդ delete(int index) որ տանք ինդեքսը, այդ ինդեքսի տակ գտնվող թիվը
     // հեռացնի մասիվից. (նոր մասիվ պետք չէ սարքել)
-    public void delete(int index) {
-        if (index - 1 > size) {
-            System.out.println("You can't delete anything from array without adding any value there");
+    public void delete(int index) {         //առանց նոր մասսիվ սարքելու չեմ կարողանւմ թիվ ջնջեմ,
+        if (index - 1 > size) {             //կամ էլ temp մասսիվ պետքա ունենամ, հետո հասցեն փոխեմ array-ի վրա
+            System.out.println("You can't delete anything from array without adding value there");
         } else {
             System.out.print("Here is the array with deleted index: ");
             for (int i = 0; i < size; i++) {
                 if (i != index) {
                     System.out.print(array[i] + " ");
-                } else {
-                    continue;
                 }
             }
         }
@@ -33,9 +31,9 @@ public class DynamicArray {
     // իսկ էղած թիվը ու կողքի բոլոր թվերը կտանի աջ, ոչ մի թիվ չի կորի
     public void addNewValue(int index, int value) {
         if (index > size) {
-            System.out.print("Your array length is less then" + "\"" + index + "\"" + ". Try to enter another index");
+            System.out.print("Your array length is less then " + "\"" + index + "\"" + ". Try to enter another index");
         } else {
-            System.out.print("Here is your array with added value under " + index + ": ");
+            System.out.print("Here is your array with added value " + "\"" + value + "\"" + " under index " + "\"" + index + "\"" + ": ");
             int[] changedArray = new int[size + 1];
             for (int i = 0; i <= index; i++) {
                 changedArray[i] = array[i];
@@ -62,43 +60,44 @@ public class DynamicArray {
             System.out.println("You can't change anything in array without adding any value there");
         } else {
             int[] changedArray = array;
-            System.out.print('\n' + "You just changed your created array and under index " + index + " now you have ");
+            System.out.print('\n' + "You just changed your created array and under index " + "\"" + index + "\"" + " now you have ");
             for (int i = 0; i < size; i++) {
                 i = index;
                 array[i] = value;
                 System.out.println(array[i] + "." + '\n');
                 break;
             }
-            System.out.print("Here are the changed array elements: ");
+            System.out.print("Here are the updated array elements: ");
             int j;
             for (j = 0; j < size; j++) {
                 System.out.print(changedArray[j] + " ");
             }
-
         }
         System.out.println("\n");
     }
 
+    // գրել մեթոդ isEmpty անունով, որը կվերադարձնի true եթե մեր DynamicArray-ի մեջ
+    //չունենանք ոչ մի էլեմենտ, իսկ եթե ունենանք՝ false.
     boolean isEmpty() {
         boolean b = false;
-        for (int i = 0; i < size; i++) {
-            if (array[i] == 0) {
+        for (int i = 0; i <= size; i++) {
+            if (size == 0) {
                 b = true;
-                System.out.print("There is no elements in array");
+                System.out.println("There are no elements in array" + '\n');
                 return b;
             } else {
-                System.out.println("There are some elements in array");
+                System.out.println("There are some elements in array" + '\n');
                 break;
             }
         }
-        System.out.println();
+
         return b;
     }
 
     //Գրել մեթոդ getByIndex անունով, որը կընդունի ինդեքս, և կվերադարձնի այդ ինդեքսի տակ ընկած թիվը, եթե չկա թող վերադարձնի 0;
-    public int getByIndex(int inputtedIndex) {
+    public int getByIndex(int inputtedIndex) { //կուզենայի switch-ով գրեի, բայց չստացվեց case-երը գրել
         for (int i = 0; i < size; i++) {
-            if (inputtedIndex == 0) {
+            if (inputtedIndex == 0) {       //առանց էս if-ի 0-ով ինդեքսը չի աշխատում
                 System.out.println("Value under inputted index " + inputtedIndex + " is: " + array[0]);
                 break;
             }
@@ -124,25 +123,22 @@ public class DynamicArray {
     // կվերադարձնի իր ինդեքսը. եթե շատ ունենք, պետք է վերադարձնել առաջինի ինդեքսը
     public int getFirstIndexByValue(int value) {
         int c = 0;
-        int index = 0;
         for (int i = 0; i < size; i++) {
             c = array[i];
             if (value == c) {
-                System.out.println("There is a match with your inputted value " + value + ".");
-
-                break;
+                System.out.println("There is a match with your inputted value " + value + ". The index is " + i);
+                return c;
             }
         }
         if (value != c) {
-            System.out.println("There isn't match with your inputted value " + value + ".");
+            System.out.println("No match detected with your inputted value " + value + ".");
         }
-
-        return index;
+        return 0;
     }
 
     public void printArray() {
         if (size == 0) {
-            System.out.println("You haven't anything in your array.");
+            System.out.println("You don't have anything in your array.");
         } else {
             System.out.print("Your created array has selected values: ");
             for (int i = 0; i < size; i++) {
