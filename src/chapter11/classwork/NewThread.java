66853041cd3eb8071ac3a433adebd023c41ff859
Narek Galstyan <narekgalstyan92@gmail.com>
2022-07-30 00:03:25 -0700

    Chapter 11 classwork, Threat examples part 1

---
+++ src/chapter11/classwork/NewThread.java
@@ -0,0 +1,18 @@
+package chapter11.classwork;
+
+public class NewThread implements Runnable {
+
+    @Override
+    public void run() {
+        try {
+            for (int i = 5; i > 0; i--) {
+                System.out.println("дочерный поток" + i);
+                Thread.sleep(500);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("дочерный поток завершен");
+
+    }
+}
---
+++ src/chapter11/classwork/ThreatDemo.java
@@ -0,0 +1,19 @@
+package chapter11.classwork;
+
+public class ThreatDemo {
+    public static void main(String[] args) {
+        Thread t = new Thread(new NewThread(), "our new threat");
+        t.start();
+
+        try {
+            for (int i = 5; i > 0; i--) {
+                System.out.println("главный поток" + i);
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("главный поток завершен");
+
+    }
+}
